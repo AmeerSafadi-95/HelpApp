08c9db476047597ddc7563204b3bafd902b520f6
AmeerSafadi-95 <ameersafadi95@gmail.com>
2020-07-27 14:27:21 +0300

    Updates

---
+++ precache-manifest.2911c0e7d802b84968c4d73e527aa256.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c68556f55e72ab99adf0b645361e826",
+    "url": "/HelpApp/index.html"
+  },
+  {
+    "revision": "76fa04c97c73cee81d2d",
+    "url": "/HelpApp/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "c6e7911361711f3399cb",
+    "url": "/HelpApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76fa04c97c73cee81d2d",
+    "url": "/HelpApp/static/js/2.2fe0f253.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HelpApp/static/js/2.2fe0f253.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6e7911361711f3399cb",
+    "url": "/HelpApp/static/js/main.c7a57fc5.chunk.js"
+  },
+  {
+    "revision": "252648febd7f1e300688",
+    "url": "/HelpApp/static/js/runtime-main.5bcf91ec.js"
+  }
+]);
